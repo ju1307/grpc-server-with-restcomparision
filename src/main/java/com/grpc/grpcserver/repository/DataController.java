085f5c634588defed202ba6697fdf98d40fb3c00
gyadav <g.yadav@dsslp.com>
2019-09-26 10:54:44 +0530

    http 2 changes

--- src/main/java/com/grpc/grpcserver/repository/DataController.java
+++ src/main/java/com/grpc/grpcserver/repository/DataController.java
@@ -1,6 +1,7 @@
 package com.grpc.grpcserver.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.grpc.grpcserver.AttItemListDTO;
-
+@SpringBootApplication
 @RestController
 public class DataController {
 
@@ -17,7 +18,7 @@ public class DataController {
 	
 	@GetMapping(value="getData")
 	public AttItemListDTO getmapping(){
-		Pageable firstPageWithTwoElements = new PageRequest(1, 1000);//.of(0, 2);
+		Pageable firstPageWithTwoElements = new PageRequest(1, 10);//.of(0, 2);
 		Page<DataEntity> list = dataRepository.findAll(firstPageWithTwoElements);
 		AttItemListDTO list1= new AttItemListDTO();
 		list1.setDataEnity(list.getContent());
--- src/main/java/com/grpc/grpcserver/service/DataBaseServiceGrpc.java
+++ src/main/java/com/grpc/grpcserver/service/DataBaseServiceGrpc.java
@@ -1,6 +1,6 @@
 package com.grpc.grpcserver.service;
 
-import java.util.List;
+import java.util.function.Consumer;
 
 import org.lognet.springboot.grpc.GRpcService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,13 +24,18 @@ public class DataBaseServiceGrpc  extends AttItemServiceGrpc.AttItemServiceImplB
 
 	@Override
 	public void getItemService(AttItem request, StreamObserver<AttItemList> responseObserver) {
-		Pageable firstPageWithTwoElements = new PageRequest(1, 1000);//.of(0, 2);
+		Pageable firstPageWithTwoElements = new PageRequest(1, 100);//.of(0, 2);
 		Page<DataEntity> list = datarepository.findAll(firstPageWithTwoElements);
-		 AttItemList.Builder responseBuilder = AttItemList.newBuilder();
-		list.getContent().forEach(dataItem->{
-			AttItem attItem = AttItem.newBuilder().setId(dataItem.getId().toString()).setItemId(dataItem.getItemId()).setItemNo(dataItem.getItemNo()).build();
-			responseBuilder.addAttItem(attItem);
+		 final AttItemList.Builder responseBuilder = AttItemList.newBuilder();
+		 
+		   list.getContent().forEach(new Consumer<DataEntity>() {
+			@Override
+			public void accept(DataEntity dataItem) { AttItem attItem =
+			   AttItem.newBuilder().setId(dataItem.getId().toString()).setItemId(dataItem.
+			   getItemId()).setItemNo(dataItem.getItemNo()).build();
+			   responseBuilder.addAttItem(attItem); }
 		});
+		  
 		AttItemList attItemList = responseBuilder.build();
 		responseObserver.onNext(attItemList);
 		 responseObserver.onCompleted();
@@ -40,15 +45,18 @@ public class DataBaseServiceGrpc  extends AttItemServiceGrpc.AttItemServiceImplB
 	public void getItemStreamService(AttItem request, StreamObserver<AttItemList> responseObserver) {
 		long count = datarepository.count();
 		count=count/1000;
-		 AttItemList.Builder responseBuilder = AttItemList.newBuilder();
-		 Pageable firstPageWithTwoElements = new PageRequest(1, 1000);//.of(0, 2);
+		 final AttItemList.Builder responseBuilder = AttItemList.newBuilder();
+		 Pageable firstPageWithTwoElements = new PageRequest(1, 100);//.of(0, 2);
 			Page<DataEntity> list = datarepository.findAll(firstPageWithTwoElements);
 		for(int i=0;i<10;i++) {
 			
 			 
-			list.getContent().forEach(dataItem->{
-				AttItem attItem = AttItem.newBuilder().setId(dataItem.getId().toString()).setItemId(dataItem.getItemId()).setItemNo(dataItem.getItemNo()).build();
-				responseBuilder.addAttItem(attItem);
+			list.getContent().forEach( new Consumer<DataEntity>() {
+				@Override
+				public void accept(DataEntity data) {
+					AttItem attItem = AttItem.newBuilder().setId(data.getId().toString()).setItemId(data.getItemId()).setItemNo(data.getItemNo()).build();
+					responseBuilder.addAttItem(attItem);
+				}
 			});
 			AttItemList attItemList = responseBuilder.build();
 			responseObserver.onNext(attItemList);	
